b933086e17f86375d177e4138163c811efaeb4f9
MarxJiao <marxjiao@gmail.com>
2017-01-08 18:14:04 +0800

    remove test file coverage

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function(config) {
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
-          'test/unit/**/*.spec.js': ['webpack', 'coverage']
+          'test/unit/**/*.spec.js': ['webpack']
         },
 
 
